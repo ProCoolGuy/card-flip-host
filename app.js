6071ee0a5d056a2d54e40798fbbcf15c94125c8d
ProCoolGuy <handsomeguy7044@gmail.com>
2022-04-11 07:25:10 -0600

    .

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const resolvePath = require('path').resolve;
+const compression = require('compression');
+const fs = require('fs');
+const routes = require('./routes');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+app.use(cors());
+app.use(compression());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cookieParser());
+
+app.use('/history', routes);
+app.use(require('express').static(resolvePath(__dirname, './build')));
+app.get('*', (req, res) => {
+  const contents = fs.readFileSync(
+    resolvePath(__dirname, './build/index.html'),
+    'utf8'
+  );
+  res.send(contents);
+});
+
+app.listen(process.env.APP_PORT || 5000, () => {
+  console.log(`Server running at 5000`);
+});
---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'gamehistory',
+  });
+  connection.connect();
+  connection.query(
+    'SELECT * FROM card ORDER BY no DESC Limit 100',
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+  connection.end();
+});
+
+router.put('/', function (req, res, next) {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'gamehistory',
+  });
+  req = req.body.req;
+  connection.connect();
+  connection.query(
+    `INSERT INTO card (player, txn, amount, choice, result, payout)  VALUES ('${req.player}', '${req.txn}',${req.amount},'${req.choice}','${req.result}',${req.payout})`,
+    (err, result, fields) => {
+      if (err) throw err;
+    }
+  );
+  connection.query(
+    'SELECT * FROM card ORDER BY no DESC Limit 100',
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+  connection.end();
+});
+
+module.exports = router;
